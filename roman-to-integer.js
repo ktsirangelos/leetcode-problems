2b8dd484fa1cbb1b89934805c621ff45c4d92932
Konstantinos Tsirangelos <91125365+ktsirangelos@users.noreply.github.com>
2023-01-26 10:17:34 +0100

    added comments

--- roman-to-integer.js
+++ roman-to-integer.js
@@ -38,7 +38,7 @@ function romanToInt(s) {
 
   transform("MMM", 3000);
   transform("MM", 2000);
-  transform("CM", 900);
+  transform("CM", 900); // changed order
   transform("M", 1000);
 
   // 100
@@ -46,7 +46,7 @@ function romanToInt(s) {
   transform("DCCC", 800);
   transform("DCC", 700);
   transform("DC", 600);
-  transform("CD", 400);
+  transform("CD", 400); // changed order
   transform("D", 500);
   transform("CCC", 300);
   transform("CC", 200);
@@ -58,11 +58,11 @@ function romanToInt(s) {
   transform("LXXX", 80);
   transform("LXX", 70);
   transform("LX", 60);
-  transform("XL", 40);
+  transform("XL", 40); // changed order
   transform("L", 50);
   transform("XXX", 30);
   transform("XX", 20);
-  transform("IX", 9);
+  transform("IX", 9); // changed order
   transform("X", 10);
 
   // 1
