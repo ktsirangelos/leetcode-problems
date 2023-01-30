598b04a5ad93c37a4f31e873b41f05ba7373225e
Konstantinos Tsirangelos <91125365+ktsirangelos@users.noreply.github.com>
2023-01-30 16:07:07 +0100

    Update 4-longest-common-prefix.js

--- 4-longest-common-prefix.js
+++ 4-longest-common-prefix.js
@@ -1,11 +1,34 @@
 "use strict";
 
-console.log(`dfdf`);
-
 const longestCommonPrefix = function (strs) {
-  strs.forEach((string, i) => {
-    console.log(string.slice(0, 1));
-  });
+  if (strs.length == 1) {
+    if (strs.includes("")) {
+      return "";
+    } else {
+      return strs[0];
+    }
+  } else {
+    let counter = 0;
+    let counterNew = 0;
+    let slicePosition = 1;
+    let currentPrefix = strs[0].slice(0, slicePosition);
+    let commonPrefix = "";
+    while (counter == counterNew && commonPrefix.length < strs[0].length) {
+      for (let j = 0; j < strs.length; j++) {
+        if (currentPrefix == strs[j].slice(0, slicePosition)) {
+          counter++;
+        }
+      }
+      if (counter == strs.length * slicePosition) {
+        commonPrefix = strs[0].slice(0, slicePosition++);
+        currentPrefix = strs[0].slice(0, slicePosition);
+        counterNew = counter;
+      }
+    }
+    if (counterNew > 0) {
+      return commonPrefix;
+    } else {
+      return "";
+    }
+  }
 };
-
-longestCommonPrefix(["flower", "flow", "flight"]);
