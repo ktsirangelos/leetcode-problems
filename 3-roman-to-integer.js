64b40d7859f453c45adf6b38b1b63ba5f2251d10
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-03-02 11:07:45 +0100

    Update 3-roman-to-integer.js

--- 3-roman-to-integer.js
+++ 3-roman-to-integer.js
@@ -1,55 +1,44 @@
 const romanToInt = function (s) {
   let int = 0;
   let string = s;
-
-  const transform = function (a, b) {
+  const transform = function ([a, b]) {
     if (string.includes(a)) {
       int += b;
       string = string.replace(a, "");
     }
   };
-
-  // 1000
-
-  transform("MMM", 3000);
-  transform("MM", 2000);
-  transform("CM", 900); // changed order
-  transform("M", 1000);
-
-  // 100
-
-  transform("DCCC", 800);
-  transform("DCC", 700);
-  transform("DC", 600);
-  transform("CD", 400); // changed order
-  transform("D", 500);
-  transform("CCC", 300);
-  transform("CC", 200);
-  transform("XC", 90);
-  transform("C", 100);
-
-  // 10
-
-  transform("LXXX", 80);
-  transform("LXX", 70);
-  transform("LX", 60);
-  transform("XL", 40); // changed order
-  transform("L", 50);
-  transform("XXX", 30);
-  transform("XX", 20);
-  transform("IX", 9); // changed order
-  transform("X", 10);
-
-  // 1
-
-  transform("VIII", 8);
-  transform("VII", 7);
-  transform("VI", 6);
-  transform("IV", 4);
-  transform("V", 5);
-  transform("III", 3);
-  transform("II", 2);
-  transform("I", 1);
-
+  const relationships = [
+    ["MMM", 3000],
+    ["MM", 2000],
+    ["CM", 900], // changed order
+    ["M", 1000],
+    ["DCCC", 800],
+    ["DCC", 700],
+    ["DC", 600],
+    ["CD", 400], // changed order
+    ["D", 500],
+    ["CCC", 300],
+    ["CC", 200],
+    ["XC", 90],
+    ["C", 100],
+    ["LXXX", 80],
+    ["LXX", 70],
+    ["LX", 60],
+    ["XL", 40], // changed order
+    ["L", 50],
+    ["XXX", 30],
+    ["XX", 20],
+    ["IX", 9], // changed order
+    ["X", 10],
+    ["VIII", 8],
+    ["VII", 7],
+    ["VI", 6],
+    ["IV", 4],
+    ["V", 5],
+    ["III", 3],
+    ["II", 2],
+    ["I", 1],
+  ];
+  relationships.forEach((array) => transform(array));
   return int;
 };
