122ee28a0d9b72717700d0874eb7719a9da77cac
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2023-03-02 10:35:39 +0100

    Update 1-two-sum.js

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,4 +1,6 @@
-"use strict";
+//Two-sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
 
 const twoSum = function (nums, target) {
   for (let a = 0; a <= nums.length; a++) {
